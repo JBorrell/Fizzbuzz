fc27be26c04cc37e6402a22f41ac7848a90ce85e
JBorrell <james.borrell92@gmail.com>
2015-11-20 18:11:37 +0000

    Rspec tests random numbers from 1-100

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,14 +4,8 @@ describe 'fizzbuzz' do
     it 'returns "fizz" when passed 3' do
         expect(fizzbuzz(3)).to eq 'fizz'
     end
-    it 'returns "fizz" when passed 12' do
-    expect(fizzbuzz(12)).to eq 'fizz'
-    end
     it 'returns "buzz" when passed 10' do
         expect(fizzbuzz(10)).to eq 'buzz'
-    end
-    it 'returns "8" when passed 8' do
-        expect(fizzbuzz(8)).to eq 8
     end
     it 'returns "fizzbuzz" when passed 15' do
         expect(fizzbuzz(15)).to eq 'fizzbuzz'
@@ -19,19 +13,26 @@ describe 'fizzbuzz' do
     it 'returns "17" when passed 17' do
         expect(fizzbuzz(17)).to eq 17
     end
-    it 'returns "fizzbuzz" when passed 30' do
-        expect(fizzbuzz(30)).to eq 'fizzbuzz'
+    
+    25.times do
+    x = rand(100)
+    if (x % 3 == 0 && x % 5 == 0)
+      it 'returns "fizzbuzz" when passed a number that is multiple of both 3 and 5' do
+         expect(fizzbuzz(x)).to eq 'fizzbuzz'
+      end
+    elsif x % 3 == 0
+      it 'returns "fizz" when passed a number that is a multiple of 3' do
+        expect(fizzbuzz(x)).to eq 'fizz'
+      end
+    elsif x % 5 == 0
+      it 'returns "buzz" when passed a number that is a multiple of 5' do
+        expect(fizzbuzz(x)).to eq 'buzz'
+      end
+    else
+      it "returns given number if not multiple of 3 or 5" do
+        expect(fizzbuzz(x)).to eq x
+      end
     end
-    it 'returns "fizz" when passed 39' do
-        expect(fizzbuzz(39)).to eq 'fizz'
     end
-    it 'returns "buzz" when passed 65' do
-        expect(fizzbuzz(65)).to eq 'buzz'
-    end
-    it 'returns "fizzbuzz" when passed 120' do
-        expect(fizzbuzz(120)).to eq 'fizzbuzz'
-    end
-    it 'returns "152" when passed 152' do
-        expect(fizzbuzz(152)).to eq 152
-    end
+  
 end
